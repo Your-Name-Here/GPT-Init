bdc5486f96f2d031c401399ba8e4e70a4ffa7f83
Your-Name-Nere <archdeacon84@gmail.com>
2024-02-25 22:56:58 -0600

    + ESLint

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 
 // @ts-check
 import 'dotenv/config'
-import { getSteps, executeStep } from './LLM.js';
-import {prompts} from './questions.js';
+import { getSteps, executeStep } from './LLM.js'
+import {prompts} from './questions.js'
 import chalk from 'chalk';
 
 (async()=>{
-  console.log()
-  console.log(chalk.yellow('AI-Powered Project Bootstrapper'))
-  console.log(chalk.gray('Answer the following questions to get started with your project.'))
-  console.log()
-  const answers = await prompts();
-
-  // console.log(chalk.green('Project Name: '), answers.ProjectName);
-  // console.log(chalk.green('Project Type: '), answers.ProjectType);
-  // console.log(chalk.green('Project Description: '), answers.ProjectDescription);
-  // console.log(chalk.green('Technology Stack: '), answers.technologyStack);
-  console.log(chalk.yellow('Parsing steps to initialize your project...'));
-  const steps = await getSteps(answers.ProjectType, answers.technologyStack);
-  const parsedSteps = JSON.parse(steps.toString());
-  parsedSteps.forEach((step, index) => {
-    console.log(chalk.green(`Step ${index + 1}: `), step);
-  })
-  console.log(chalk.yellow('Executing steps...'));
-  for(const step of parsedSteps){
-    console.log(chalk.green('Executing step: '), step);
-    await executeStep(step, answers.ProjectType, answers.technologyStack);
-  }
-} )();
+	console.log()
+	console.log(chalk.yellow('GPT-Init'), chalk.gray('AI-Powered Project Bootstrapper'))
+	console.log(chalk.gray('Answer the following questions to get started with your project.'))
+	console.log()
+	// Ask initial Questions
+	const answers = await prompts()
+	
+	console.log(chalk.yellow('Parsing steps to initialize your project...'))
+	
+	// Get the steps to initialize the project
+	const steps = await getSteps(answers.ProjectType, answers.technologyStack)
+	
+	const parsedSteps = JSON.parse(steps.toString())
+	
+	parsedSteps.forEach((step, index) => {
+		console.log(chalk.green(`Step ${index + 1}: `), step)
+	})
+	
+	console.log(chalk.yellow('Executing steps...'))
+	
+	for(const step of parsedSteps){
+		console.log(chalk.green('Executing step: '), step)
+		await executeStep(step, answers.ProjectType, answers.technologyStack)
+	}
+} )()
--- questions.js
+++ questions.js
@@ -1,84 +1,84 @@
-import { input,  } from '@inquirer/prompts';
-import checkbox, { Separator } from '@inquirer/checkbox';
-import select  from '@inquirer/select';
+import { input,  } from '@inquirer/prompts'
+import checkbox, { Separator } from '@inquirer/checkbox'
+import select  from '@inquirer/select'
 
 export const ask = async (question) => {
-   return await input({ message: question });
+	return await input({ message: question })
 }
 
 export async function prompts(){
-    const ProjectName = await input({ message: 'What is the name of your project?' });
+	const ProjectName = await input({ message: 'What is the name of your project?' })
 
-    const ProjectType = await select({
-        message: 'What type of project are you creating?',
-        choices: [
-        {
-            name: 'Node.js Project',
-            value: 'node',
-            description: 'Node.js is a JavaScript runtime built on Chrome\'s V8 JavaScript engine.',
-        },
-        {
-            name: 'Web Project',
-            value: 'web',
-            description: 'A web project is a project that is meant to be run in a web browser.',
-        },
-        new Separator(),
-        {
-            name: 'Native Project',
-            value: 'native',
-            description: 'A native project is a project that is meant to be run on a native platform. Think iOS, Android, or Windows.',
-        },
-        ]
-    });
+	const ProjectType = await select({
+		message: 'What type of project are you creating?',
+		choices: [
+			{
+				name: 'Node.js Project',
+				value: 'node',
+				description: 'Node.js is a JavaScript runtime built on Chrome\'s V8 JavaScript engine.',
+			},
+			{
+				name: 'Web Project',
+				value: 'web',
+				description: 'A web project is a project that is meant to be run in a web browser.',
+			},
+			new Separator(),
+			{
+				name: 'Native Project',
+				value: 'native',
+				description: 'A native project is a project that is meant to be run on a native platform. Think iOS, Android, or Windows.',
+			},
+		]
+	})
 
-    const ProjectDescription = await input({ message: 'Please provide a description of your project.' });
+	const ProjectDescription = await input({ message: 'Please provide a description of your project.' })
 
-    const technologyStack = await checkbox({
-        message: 'What technology stack will you be using?',
-        choices: [
-            new Separator('Languages'),
-            { name: "Typescript", value: "typescript"},
-            { name: "Javascript (ESM)", value: "javascript (ESM)"},
-            { name: "Javascript (CJS)", value: "javascript (CJS)"},
-            new Separator('Frontend Frameworks'),
-            // { name: 'React', value: 'react' },
-            // { name: 'Vue', value: 'vue' },
-            // { name: 'Angular', value: 'angular' },
-            { name: 'Svelte', value: 'svelte' },
-            { name: 'Electron', value: 'electron' },
-            { name: 'Other Framework Option', value: 'Unknown Frontend Framework' },
-            new Separator('Backend Frameworks'),
-            { name: 'Express', value: 'express' },
-            // { name: 'Koa', value: 'koa' },
-            // { name: 'Nest', value: 'nest' },
-            new Separator('Databases'),
-            { name: 'MongoDB', value: 'mongodb' },
-            { name: 'MySQL', value: 'mysql' },
-            // { name: 'PostgreSQL', value: 'postgresql' },
-            { name: 'Other DB Option', value: 'Unknown Database' },
-            new Separator('Testing Frameworks'),
-            // { name: 'Jest', value: 'jest' },
-            { name: 'Mocha', value: 'mocha' },
-            { name: 'Chai', value: 'chai' },
-            { name: 'Other Testing Framework Option', value: 'Unknown Testing Framework' },
-            new Separator('CI/CD Tools'),
-            { name: 'Github Actions', value: 'github actions' },
-            // { name: 'Travis CI', value: 'travis ci' },
-            // { name: 'Jenkins', value: 'jenkins ci' },
-            { name: 'Other CI/CD Tool Option', value: 'Unknown CI/CD Tool' },
-            // new Separator('Containerization Tools'),
-            // { name: 'Docker', value: 'docker' },
-            // { name: 'Kubernetes', value: 'kubernetes' },
-            new Separator('Cloud Providers'),
-            { name: 'Redis', value: 'redis'},
-            // { name: 'AWS', value: 'aws' },
-            // { name: 'Azure', value: 'azure' },
-            // { name: 'Google Cloud', value: 'google cloud' },
-            new Separator('Other Tools'),
-            { name: 'Eslint', value: 'eslint' },
-            // { name: 'Prettier', value: 'prettier' },
-            // { name: 'Husky', value: 'husky' },
-        ],
-    });
-    return {ProjectName, ProjectType, ProjectDescription, technologyStack}
+	const technologyStack = await checkbox({
+		message: 'What technology stack will you be using?',
+		choices: [
+			new Separator('Languages'),
+			{ name: 'Typescript', value: 'typescript'},
+			{ name: 'Javascript (ESM)', value: 'javascript (ESM)'},
+			{ name: 'Javascript (CJS)', value: 'javascript (CJS)'},
+			new Separator('Frontend Frameworks'),
+			// { name: 'React', value: 'react' },
+			// { name: 'Vue', value: 'vue' },
+			// { name: 'Angular', value: 'angular' },
+			{ name: 'Svelte', value: 'svelte' },
+			{ name: 'Electron', value: 'electron' },
+			{ name: 'Other Framework Option', value: 'Unknown Frontend Framework' },
+			new Separator('Backend Frameworks'),
+			{ name: 'Express', value: 'express' },
+			// { name: 'Koa', value: 'koa' },
+			// { name: 'Nest', value: 'nest' },
+			new Separator('Databases'),
+			{ name: 'MongoDB', value: 'mongodb' },
+			{ name: 'MySQL', value: 'mysql' },
+			// { name: 'PostgreSQL', value: 'postgresql' },
+			{ name: 'Other DB Option', value: 'Unknown Database' },
+			new Separator('Testing Frameworks'),
+			// { name: 'Jest', value: 'jest' },
+			{ name: 'Mocha', value: 'mocha' },
+			{ name: 'Chai', value: 'chai' },
+			{ name: 'Other Testing Framework Option', value: 'Unknown Testing Framework' },
+			new Separator('CI/CD Tools'),
+			{ name: 'Github Actions', value: 'github actions' },
+			// { name: 'Travis CI', value: 'travis ci' },
+			// { name: 'Jenkins', value: 'jenkins ci' },
+			{ name: 'Other CI/CD Tool Option', value: 'Unknown CI/CD Tool' },
+			// new Separator('Containerization Tools'),
+			// { name: 'Docker', value: 'docker' },
+			// { name: 'Kubernetes', value: 'kubernetes' },
+			new Separator('Cloud Providers'),
+			{ name: 'Redis', value: 'redis'},
+			// { name: 'AWS', value: 'aws' },
+			// { name: 'Azure', value: 'azure' },
+			// { name: 'Google Cloud', value: 'google cloud' },
+			new Separator('Other Tools'),
+			{ name: 'ES Lint', value: 'eslint' },
+			// { name: 'Prettier', value: 'prettier' },
+			// { name: 'Husky', value: 'husky' },
+		],
+	})
+	return {ProjectName, ProjectType, ProjectDescription, technologyStack}
 }
